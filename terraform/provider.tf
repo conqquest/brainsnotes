7a7e5d4912ba8e36bc3fd0d3e81c70d59f818f34
Adarsh <tenminusthreeseven@gmail.com>
2026-09-06 18:26:31 +0530

    ye : terraform files creation started

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,9 @@
 terraform {
-    provider
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+    required_version = ">= 1.5.0"
 }
