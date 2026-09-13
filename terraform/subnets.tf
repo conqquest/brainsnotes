3cb63b49a4de4532fa1797ab208929b1291c22c8
Adarsh <tenminusthreeseven@gmail.com>
2026-09-13 18:18:41 +0530

    ye: subnets created

---
+++ terraform/subnets.tf
@@ -0,0 +1,49 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Public Subnet - AZ 1
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "brains-public-a"
+  }
+}
+
+# Public Subnet - AZ 2
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "brains-public-b"
+  }
+}
+
+# Private Subnet - AZ 1
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "brains-private-a"
+  }
+}
+
+# Private Subnet - AZ 2
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "brains-private-b"
+  }
+}
